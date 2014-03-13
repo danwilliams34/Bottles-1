75f61833b4d952f0fa7e6388d7fd8f78ceb1ea44
Buk Lau <daniel.williams1@ntlworld.com>
2014-03-13 10:16:08 +0000

    Update runtests.py

--- runtests.py
+++ runtests.py
@@ -1,3 +1,14 @@
+"in this file we shall test bottles.py"
+import unittest
+import bottles
+
+class tests (unittest.TestCase):
+    def test1(self):
+        pass
+    if __name__ == '__main__'=
+        unittest.main()
+    
+        
 def test1(self):
     song_expected_last_word = 'floor'
     song_actual_last_word = bottles.song[-5:]
