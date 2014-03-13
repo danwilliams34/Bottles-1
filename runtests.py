634f5e855df7e3081b5b3daee8d1cd379dbd6e82
Buk Lau <daniel.williams1@ntlworld.com>
2014-03-13 10:08:44 +0000

    Update runtests.py

--- runtests.py
+++ runtests.py
@@ -1,1 +1,4 @@
-ad
+def test1(self):
+    song_expected_last_word = 'floor'
+    song_actual_last_word = bottles.song[-5:]
+    self.assertEquals(song_expected_last_word, song_actual_last_word, "last word is wrong, Test 1 fails")
